3675490ec2823ac04c9ac6bb323dab4681ef09cb
thomsedavi <uniquelydave@gmail.com>
2016-12-11 18:34:10 +1300

    added some non-functional placeholder components

--- poetry.py
+++ poetry.py
@@ -1,15 +1,43 @@
-import tkinter
 from tkinter import *
 
-root = tkinter.Tk()
+root = Tk()
 
-text = Text(root, width=80, height=4)
-text.insert(INSERT, "Hello world, I guess?")
-text.configure(state="disabled")
-text.pack()
+textFrame = Frame(root)
+textFrame.pack()
 
-entry = Text(root, width=80, height=4)
-entry.pack()
+buttonFrame = Frame(root)
+buttonFrame.pack()
+
+def clearFrames():
+  for child in textFrame.winfo_children():
+    child.pack_forget()
+  for child in buttonFrame.winfo_children():
+    child.pack_forget()
+
+def newSeedDef():
+  clearFrames()
+  seed.delete(0, END)
+
+  seed.pack()
+  newSeed.pack(side=LEFT)
+  saveSeed.pack(side=LEFT)
+
+def saveSeedDef():
+  text = seed.get()
+  print(text)
+
+  clearFrames()
+
+  message.pack()
+  newSeed.pack()
+
+message = Label(textFrame, text="Message!", width=80)
+seed = Entry(textFrame, width = 80)
+newSeed = Button(buttonFrame, text="New Seed", command=newSeedDef)
+saveSeed = Button(buttonFrame, text="Save Seed", command=saveSeedDef)
+
+message.pack()
+newSeed.pack()
 
 root.mainloop()
 
